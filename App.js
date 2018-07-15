84e375a70006e4d1fa6f191b3bbbb4c2a9d8574a
吉村明史 <awakewalker@awakewalker.local>
2018-07-15 15:33:44 +0900

    Refactoring MemoList

--- App.js
+++ App.js
@@ -1,54 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import BodyText from './src/elements/BodyText';
+import Appbar from './src/components/Appbar';
+import MemoList from './src/components/MemoList';
+import CircleButton from './src/elements/CircleButton';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.appbar}>
-          <View>
-            <Text style={styles.appbarTitle}>Memo</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoList}>
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoAddButton}>
-          <Text style={styles.memoAddButtonTitle}>+</Text>
-        </View>
-
+        <Appbar />
+        <MemoList />
+        <CircleButton>+</CircleButton>
       </View>
     );
   }
@@ -61,63 +24,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 78,
-  },
-  memoAddButton: {
-    position: 'absolute',
-    bottom: 32,
-    right: 32,
-    width: 48,
-    height: 48,
-    backgroundColor: '#E31676',
-    borderRadius: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, heigth: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-  memoAddButtonTitle: {
-    fontSize: 24,
-    lineHeight: 24,
-    color: '#fff',
-  },
-  memoList: {
-    flex: 1,
-    width: '100%',
-  },
-  memoListItem: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    backgroundColor: '#fff',
-  },
-  memoDate: {
-    fontSize: 12,
-    color: '#a2a2a2',
-  },
-  memoTitle: {
-    fontSize: 18,
-    marginBottom: 4,
-  },
-  appbar: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 78,
-    paddingTop: 30,
-    backgroundColor: '#265366',
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.5,
-    shadowRadius: 3,
-    zIndex: 10,
-  },
-  appbarTitle: {
-    color: '#fff',
-    fontSize: 18,
   },
 });
