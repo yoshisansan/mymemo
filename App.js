63a842f66e57476c77f94acdc5ed47d2acafe9be
吉村明史 <awakewalker@softbank221109252023.bbtec.net>
2018-07-16 01:20:39 +0900

    Style MemoDetailScreen

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoListScreen />
+        <MemoDetailScreen />
       </View>
     );
   }
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFDF6',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingTop: 78,
   },
 });
