54634e706e8ab64967d5a70400644340dab8c580
吉村明史 <awakewalker@awakewalker.local>
2018-07-28 23:00:13 +0700

    Stor memos with cloud store

--- App.js
+++ App.js
@@ -4,10 +4,14 @@ import firebase from 'firebase';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
-
 import ENV from './env.json';
+
+// eslint-disable-next-line
+require("firebase/firestore");
+
 
 const config = {
   apiKey:            ENV.FIREBASE_API_KEY,
@@ -25,6 +29,7 @@ const App = StackNavigator({
   Home:             { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit:   { screen: MemoEditScreen },
+  MemoCreate: { screen: MemoCreateScreen },
 }, {
   navigationOptions: {
     headerTitle: 'Memot',
